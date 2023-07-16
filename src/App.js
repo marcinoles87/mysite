0c93e264287fc1ee603329e0e869a5877d1ea097
marcinoles87 <marcinoles87@gmail.com>
2023-07-16 10:57:10 +0200

    add section

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import Navigation from './components/Navigation';
 import Header from './components/Header';
 import Main from './components/Main';
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
+    
     <div className="App">
-      <Navigation></Navigation>
-      <Header></Header>
-      <Main></Main>
+
+        <div className='app-container'>
+
+            <Navigation></Navigation>
+            <Header></Header>
+            <Main></Main>
+
+        </div>
+    
     </div>
   );
 }
