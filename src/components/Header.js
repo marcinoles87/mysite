b95924f1ce65475139dd9c7ebc198fdfa04ca593
marcinoles87 <marcinoles87@gmail.com>
2023-07-16 11:28:27 +0200

    add nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
+import profil from './img/minsc.png'
+
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header-container'>
+      <div className='header-img'>
+        <img className='profil-img' src={profil} alt='profil'></img>
+      </div>
+
+      <div className='header-info'>
+        <h1 className='header-info-text'>Info About My Self</h1>
+      </div>
+    </div>
   )
 }
 
