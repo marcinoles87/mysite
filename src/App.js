fc96b238266b59c48d91a6869fc53b56cc034cd8
marcinoles87 <marcinoles87@gmail.com>
2023-07-17 14:51:44 +0200

    add about section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Navigation from './components/Navigation';
 import Header from './components/Header';
 import Main from './components/Main';
+import About from './components/About';
 
 import './App.scss';
 
@@ -13,11 +14,16 @@ function App() {
 
         <div className='app-container'>
 
+          
             <Navigation></Navigation>
             <Header></Header>
             <Main></Main>
 
         </div>
+
+        <div className='about-container'>
+              <About></About>
+          </div>
     
     </div>
   );
