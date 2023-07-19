783f0f340620ed224ceeaf8f4615fdd89757b4ca
marcinoles87 <marcinoles87@gmail.com>
2023-07-19 05:38:32 +0200

    add freetime section

--- src/components/Freetime.js
+++ src/components/Freetime.js
@@ -7,7 +7,16 @@ function Freetime() {
     <div className='freetime-container'>
         <h1>Freetime - rest , piece , energy</h1>
         <ul className='freetime-list'>
-            <li className='freetime-list-item'><p className='freetime-text'>Family</p></li>
+            <li className='freetime-list-item'>
+              <p className='freetime-text'>Family</p>
+              <p className='freetime-text-more'>
+                -relaks z moim dziecmi 
+                -wieczory z zona :)
+                -spacery
+              </p>
+              </li>
+
+
             <li className='freetime-list-item'><p className='freetime-text'>Sport</p></li>
             <li className='freetime-list-item'><p className='freetime-text'>Movie</p></li>
             <li className='freetime-list-item'><p className='freetime-text'>Foods</p></li>
