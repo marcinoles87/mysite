945d76f882ae4dea175cd798c5b243db4ff10d5e
marcinoles87 <marcinoles87@gmail.com>
2023-07-19 14:54:55 +0200

    add project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,9 +13,9 @@ function Projects() {
       <div className='project-item'>
         <ul>
           <li><p>1</p> motivation</li>
-          <li><p>1</p> team player</li>
-          <li><p>1</p> hard working</li>
-          <li><p>1</p> focus on sucess</li>
+          <li><p>2</p> team player</li>
+          <li><p>3</p> hard working</li>
+          <li><p>4</p> focus on sucess</li>
          
         </ul>
       </div>
