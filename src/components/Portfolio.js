07c70df842296191caecca9863b401f03dd9bf95
marcinoles87 <marcinoles87@gmail.com>
2023-07-21 05:50:12 +0200

    work with portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,7 +5,43 @@ import './portfolio.scss';
 function Portfolio() {
   return (
     <div className='portfolio-container'>
-        <h1> Portfolio</h1>
+        <h1 className='portfolio-name'> Portfolio</h1>
+
+        <div className='portfolio-projects'>
+            
+            <div className='portfolio-description'>
+                <h1>Name</h1>
+                <p>Description</p>
+            </div>
+           
+            <div className='portfolio-link'>
+                <a href='#' ></a>
+            </div>
+        </div>
+
+        <div className='portfolio-projects'>
+            
+            <div className='portfolio-description'>
+                <h1>Name</h1>
+                <p>Description</p>
+            </div>
+           
+            <div className='portfolio-link'>
+                <a href='#' ></a>
+            </div>
+        </div>
+
+        <div className='portfolio-projects'>
+            
+            <div className='portfolio-description'>
+                <h1>Name</h1>
+                <p>Description</p>
+            </div>
+           
+            <div className='portfolio-link'>
+                <a href='#' ></a>
+            </div>
+        </div>
 
         <div className='portfolio-projects'>
             
