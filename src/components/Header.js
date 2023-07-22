46e0245fe3375f49633b9c2d55a28fdcf5c8e8ed
marcinoles87 <marcinoles87@gmail.com>
2023-07-22 06:42:34 +0200

    work with portfolio section

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,7 @@ function Header() {
       <div className='header-info'>
         <img src={logoGh} alt='github-logo'></img>
         <img src={logoLn} alt='linkedin-logo'></img>
-        
+        <i class="fa-brands fa-facebook fa-lg"></i>
 
         <h1 className='header-info-text'>Hello , I'm Marcin Oles</h1>
         <p className='header-info-text2'> This is my personal website from 2023</p>
