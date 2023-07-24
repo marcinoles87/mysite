8c91b197546ce49744127fd4abe304bd9934dc36
marcinoles87 <marcinoles87@gmail.com>
2023-07-24 11:45:53 +0200

    work scrool

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,15 +16,16 @@ function Header() {
       </div>
 
       <div className='header-info'>
-        <img src={logoGh} alt='github-logo'></img>
-        <img src={logoLn} alt='linkedin-logo'></img>
-        <img src={logoFb} alt='linkedin-logo'></img>
+
         
 
         <h1 className='header-info-text'>READY TO WORK</h1>
         <h2>Hi there , Im Marcin !</h2>
         <p className='header-info-text2'> FUTURE FRONTEND - DEVELOPER</p>
-        <button className='header-button'>More Info </button>
+        {/* <button className='header-button'>More Info </button> */}
+        <img src={logoGh} alt='github-logo'></img>
+        <img src={logoLn} alt='linkedin-logo'></img>
+        <img src={logoFb} alt='linkedin-logo'></img>
       </div>
 
 
