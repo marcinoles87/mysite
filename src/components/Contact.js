e212ad2951bf8e6e06e28f4dfcd9e65a601a0229
marcinoles87 <marcinoles87@gmail.com>
2023-07-29 08:12:10 +0200

    work with contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,14 +2,19 @@ import React from 'react'
 
 import './contact.scss'
 
-import contactImg from './img/minsc.png'
+import contactImg from './img/minsc-nobg.png'
 
 function Contact() {
   return (
     <div className='contact-container'>
+        <h1>Contact</h1>
         <img src={contactImg}></img>
         <h2> Marcin Oles</h2>
-        <h3> </h3>
+        <div className='contact-link'>
+        <i class="fa-regular fa-phone"></i>
+        <i class="fa-solid fa-address-card fa-lg"></i>
+        <i class="fa-solid fa-address-card fa-lg"></i>
+        </div>
     </div>
   )
 }
