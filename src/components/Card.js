da953c10e03db1c231343508bf41cc94934214ef
marcinoles87 <marcinoles87@gmail.com>
2023-07-30 16:41:28 +0200

    work with contact section

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,6 +6,8 @@ import './card.scss'
 function Card({method ,text , img , icon}) {
   return (
     <div className='card-container'>
+
+      
         
         <img src={img} alt='card'></img>
         <h1>{method}</h1>
