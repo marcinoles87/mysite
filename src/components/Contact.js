f18c8c3c28597297484bba0ce67e3074e2457bf9
marcinoles87 <marcinoles87@gmail.com>
2023-07-30 09:07:05 +0200

    work with contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,16 +5,26 @@ import './contact.scss'
 import contactImg from './img/minsc-nobg.png'
 
 function Contact() {
+
+  const handleClick = (e) => {
+    const div = document.querySelector('contact-info')
+
+  }
   return (
     <div className='contact-container'>
         <h1>Contact</h1>
         <img src={contactImg}></img>
         <h2> Marcin Oles</h2>
+
         <div className='contact-link'>
-        <i class="fa-regular fa-phone"></i>
-        <i class="fa-solid fa-address-card fa-lg"></i>
-        <i class="fa-solid fa-address-card fa-lg"></i>
+
+            <i class="fa-regular fa-phone" onClick={handleClick}></i>
+            <i class="fa-solid fa-address-card fa-lg " onClick={handleClick}></i>
+            <i class="fa-solid fa-address-card fa-lg" onClick={handleClick}></i>
+
         </div>
+
+             <div className='contact-info'></div>
     </div>
   )
 }
