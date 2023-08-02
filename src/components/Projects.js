50335efe6da6b524033a718ff2fb0d4086c7526b
marcinoles87 <marcinoles87@gmail.com>
2023-08-02 21:09:09 +0200

    work with css

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -34,7 +34,7 @@ function Projects() {
           
             <img className='project-githubwork' src={githubwork} alt='github-work'></img>
             <i class="fa-solid fa-circle-down fa-2xl"></i>
-            <a href='https://github.com/marcinoles87'>see me on Github</a>
+            <a href='https://github.com/marcinoles87'>See My work on Github</a>
           </div>
       </div>
         
