589d7a5737e931e772e516ecb434733fc66fe586
marcinoles87 <marcinoles87@gmail.com>
2025-09-26 16:46:11 +0200

    add new info

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,10 +5,12 @@ function Skills() {
 
     const handleShow = () => {
         const projectSkill = document.querySelector('.project-skill-person')
+        console.log(projectSkill)
         projectSkill.classList.toggle('showSkill')
     }
   return (
        <div class="project-items">
+        <h1>Skill</h1>
         <div className='project-skill-person' onClick={handleShow}> 
                     <h1>Person skills</h1>
                     <h3>Team Player</h3>
